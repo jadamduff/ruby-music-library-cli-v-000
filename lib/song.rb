702a388d29b3066a06f4528fec0a846156e5b3c6
Adam Duff <j.adam.duff@gmail.com>
2018-08-19 03:31:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,14 @@ class Song
     self.all.find {|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      Song.new(name)
+    end
+  end
+
   def save
     self.class.all << self
   end
