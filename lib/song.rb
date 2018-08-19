74ee93e847043af57e525387076bfd427f001439
Adam Duff <j.adam.duff@gmail.com>
2018-08-19 03:17:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist
     self.save
